9e3551b057e4cf52ebb08fe369eb0e3f4b814a09
Pekka-Legend <CalebCampo41@gmail.com>
2025-04-13 13:28:54 -0400

    update

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Photon.Pun;
+public class LoginManager : MonoBehaviourPunCallbacks
+{
+    public GameObject[] screens;
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private GameObject[] changeScreen(GameObject[] screens)
+    {
+        for (int i = 0; i < screens.Length; i++)
+        {
+            if (i != index)
+                screens[i].SetActive(false);
+            else
+                screens[i].SetActive(true);
+        }
+        return screens;
+    }
+    public override void OnConnectedToMaster()
+    {
+        index++;
+        screens = changeScreen(screens);
+    }
+}
