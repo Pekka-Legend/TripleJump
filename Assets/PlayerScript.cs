ab318b1e7ba5633b250ed19b96dfec0e23fa0c71
Pekka-Legend <CalebCampo41@gmail.com>
2025-04-15 15:38:14 -0400

    a

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Runtime.CompilerServices;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,47 +13,115 @@ public class PlayerScript : MonoBehaviour
     private int index = 0;
     public GameObject bg;
     public TextMeshProUGUI key;
-    private bool moveDir = false; //false = left, true = right
+    private int moveDir = 0; //0 = left, 1 = right, 2 = up, 3 = down
     private float fullTimer = 0;
+    private float steps = 0;
+    public float speed = 0;
+    private bool shouldStart = false;
+    private float ySpeed = 0;
+    private float jumpMultiplier = 1;
+    public float multiplyDamper = 0;
+    private float inputDir = -1;//-1 is no input (all others same as moveDir)
+    private bool canRecieveInput = true;
+    public float gravity = 9.8f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()//-1.38f->22.96f
     {
-
-        if (index >= 1)// this will be messed up when I go to add in the actual jump part
+        Debug.Log(jumpMultiplier);
+        if (shouldStart)
         {
-            bg.SetActive(true);
-            if (moveDir) key.text = "A";
-            else key.text = "D";
-            //in here do some sort of speed calculation and detect actual button presses
-            //probably do 3 "run throughs" before the competition to guage the player's average run through distance (this will change based on their step timing)
-            //could also just slow down the animation speed but that would make it look bad
-            //or just have it only impact jump height (probably easiest and best)
+            if (steps >= 14)
+            {
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y + ySpeed * Time.deltaTime);
+                if (transform.position.y > 3.17)
+                {
+                    index = 3;
+                    GetComponent<SpriteRenderer>().sprite = sprites[index];
+                }
+                else
+                {
+                    transform.position = new Vector2(transform.position.x, 3.17f);
+                    ySpeed = jumpMultiplier;
+                }
+                ySpeed-= gravity * Time.deltaTime;
+            }
+            else
+            {
+                if (index >= 0 && canRecieveInput && steps % 2 == 0)//make some sort of variable called "input was processed" for each step
+                {
+                    bg.SetActive(true);
+                    if (moveDir == 0) key.text = "A";
+                    else if (moveDir == 1) key.text = "D";
+                    else if (moveDir == 2) key.text = "W";
+                    else if (moveDir == 3) key.text = "S";
+
+                    
+                    if (Input.GetKey(KeyCode.A)) inputDir = 0;
+                    else if (Input.GetKey(KeyCode.D)) inputDir = 1;
+                    else if (Input.GetKey(KeyCode.W)) inputDir = 2;
+                    else if (Input.GetKey(KeyCode.S)) inputDir = 3;
+                    else inputDir = -1;
+                    if (inputDir != -1)
+                    {
+                        if (inputDir == moveDir)
+                        {
+                            
+                            jumpMultiplier += 1 / ((fullTimer - swapTime) + .25f);//this makes it so that the closer you are to the when the button shows up (which happens after the first animation frame ie swap time being used) the better jump Multiplier you get
+                        }
+                        else
+                        {
+                            jumpMultiplier /= 2;
+                        }
+
+                        canRecieveInput = false;
+                    }
+
+                }
+                else
+                {
+                    bg.SetActive(false);
+                    key.text = " ";
+                }
+                if (timer > swapTime)
+                {
+                    index++;
+                    
+                    if (index > 2)
+                    {
+                        index = 0;
+                        
+                        fullTimer = 0;
+                        steps++;
+                        if (steps % 2 == 0)
+                        {
+                            moveDir = (int)Random.Range(0f, 3f);
+                            canRecieveInput = true;
+                        }
+                        
+                        
+
+                    }
+                    GetComponent<SpriteRenderer>().sprite = sprites[index];
+                    timer = 0;
+                    
+
+                }
+                timer += Time.deltaTime;
+                fullTimer += Time.deltaTime;
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+            }
         }
         else
         {
-            bg.SetActive(false);
-            key.text = " ";
+            if (Input.GetKeyDown(KeyCode.W)) shouldStart = true;
         }
-        if (timer > swapTime)
-        {
-            index++;
-            if (index > 2)
-            {
-                index = 0;
-                moveDir = !moveDir;
-                fullTimer = 0;
-            }
-            GetComponent<SpriteRenderer>().sprite = sprites[index];
-            timer = 0;
-        }
-        timer += Time.deltaTime;
-        fullTimer += Time.deltaTime;
+
+        
 
     }
 }
